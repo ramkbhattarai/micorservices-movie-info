8b4b6b503318ce86810ccf18c2d437a8ba0d071e
Ram <ramkb466@gmail.com>
2020-03-06 12:27:46 -0500

    added controller for the movie

---
+++ src/main/java/com/rkb/resources/MovieResource.java
@@ -0,0 +1,19 @@
+package com.rkb.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rkb.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	@GetMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "test");
+	}
+
+}
