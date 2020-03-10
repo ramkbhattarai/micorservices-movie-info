b23961bf2a482d9fb319c656757689f7d93e3351
Ram <ramkb466@gmail.com>
2020-03-10 13:01:45 -0400

    added api call

--- src/main/java/com/rkb/models/Movie.java
+++ src/main/java/com/rkb/models/Movie.java
@@ -2,33 +2,40 @@ package com.rkb.models;
 
 public class Movie {
 
-	private String movieId;
-	private String name;
-	public Movie() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Movie(String movieId, String name) {
-		super();
-		this.movieId = movieId;
-		this.name = name;
-	}
-	public String getMovieId() {
-		return movieId;
-	}
-	public void setMovieId(String movieId) {
-		this.movieId = movieId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	public String toString() {
-		return "Movie [movieId=" + movieId + ", name=" + name + "]";
-	}
+	 private String movieId;
+	    private String name;
+	    private String description;
+
+	    public Movie(String movieId, String name, String description) {
+	        this.movieId = movieId;
+	        this.name = name;
+	        this.description = description;
+	    }
+
+	    public String getMovieId() {
+	        return movieId;
+	    }
+
+	    public void setMovieId(String movieId) {
+	        this.movieId = movieId;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getDescription() {
+	        return description;
+	    }
+
+	    public void setDescription(String description) {
+	        this.description = description;
+	    }
+	
 	
 	
 }
--- src/main/java/com/rkb/resources/MovieResource.java
+++ src/main/java/com/rkb/resources/MovieResource.java
@@ -1,19 +1,31 @@
 package com.rkb.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.rkb.models.Movie;
+import com.rkb.models.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 	
-	@GetMapping("/{movieId}")
-	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-		return new Movie(movieId, "test name");
-	}
+	@Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+
+    }
 
 }
